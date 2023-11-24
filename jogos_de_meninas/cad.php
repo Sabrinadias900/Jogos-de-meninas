a403a6bb62d157acf94feeeaef758ac79dbea16b
Sabrinadias900 <151972768+Sabrinadias900@users.noreply.github.com>
2023-11-24 19:54:13 -0300

    Add files via upload

---
+++ jogos_de_meninas/cad.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Jogos</title>
+<link rel="stylesheet" href="cad.css">
+</head>
+<body>
+<header>
+
+</header>
+<section>
+<tr>
+
+
+<form action="cad.php" method="get">
+<h1>Jogos</h1>
+
+<div class="container">
+<div class="jogos">
+
+<div class="jogo1">
+<img src="imagem/jogo1.jpg" alt="Pinup Facial Beauty">
+<a href="https://www.friv.com/z/games/pinupfacialbeauty/game.html?c">Pinup Facial Beauty</a>
+</div>
+
+<div class="jogo2">
+<img src="imagem/jogo2.jpg" alt="Fashion Designer World Tour">
+<a href="https://www.friv.com/z/games/fashiondesignerworldtour/game.html?c">Fashion Designer World Tour</a>
+</div>
+
+<div class="jogo3">
+<img src="imagem/jogo3.jpg" alt="Fashion Designer New York">
+<a href="https://www.friv.com/z/games/fashiondesignernewyork/game.html?c">Fashion Designer New York</a>
+</div>
+
+<div class="jogo4">
+<img src="imagem/jogo4.jpg" alt="Sleeping Princess Nails Spa">
+<a href="https://www.friv.com/z/games/sleepingprincessnailsspa/game.html?c">Sleeping Princess Nails Spa</a>
+</div>
+
+</div>
+
+<div class="jogos1">
+
+<div class="jogo5">
+<img src="imagem/jogo5.jpg" alt="Dora's Magical Garden">
+<a href="https://www.friv.com/z/games/dorasmagicalgarden/game.html?c">Dora's Magical Garden</a>
+</div>
+
+<div class="jogo6">
+<img src="imagem/jogo6.jpg" alt="Funny Haircut">
+<a href="https://www.friv.com/z/funnyhaircut/game.html?c">Funny Haircut</a>
+</div>
+
+<div class="jogo7">
+<img src="imagem/jogo7.jpg" alt="Incredible Ice Cream Inventor">
+<a href="https://www.friv.com/z/incredibleicecreaminventor/game.html?c">Incredible Ice Cream Inventor</a>
+</div>
+
+<div class="jogo8">
+<img src="imagem/jogo8.jpg" alt="Funny Troat Surgery 2">
+<a href="https://www.friv.com/z/games/funnytroatsurgery2/game.html?c">Funny Troat Surgery 2</a>    
+</div>
+
+</div>
+</div>
+</tr>
+    
+<a href="javascript:history.go(-1)" class="neon">Voltar</a>
+</form>
+</section>
+</body>
+</html>
